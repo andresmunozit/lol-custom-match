bab67913942da88ffd9332fd3296ca6b51dfe4c2
Andres Munoz <am@andresmunozit.com>
2020-05-14 18:35:55 -0500

    Add match router

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 const express = require('express');
 const path = require('path');
-const { players } = require('./utils/combinator');
+const { getPlayers, balance, balanceByMastery } = require('./utils/balancer');
 
 const app = express();
 
@@ -9,9 +9,18 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 
+app.get('/match', async (req, res, next) => {
+    const region = req.query.region;
+    const sumNames = req.query.sumNames;
 
+    const players = await getPlayers(region, sumNames);
+    const balancedByMastery = balanceByMastery(players);
+    // const match = balance(players);
+
+    res.json(balancedByMastery);
+});
 
 const port = process.env.PORT;
 app.listen( port, () => {
-    console.log(`Application is running in port ${port}`);
+    console.log(`Application is running in port ${port}...`);
 });

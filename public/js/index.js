2f07fbc2dc08d56300faa086edcc43ec8c883115
Andres Munoz <am@andresmunozit.com>
2020-05-15 02:35:35 -0500

    Lock and clean the UI on the match event

--- public/js/index.js
+++ public/js/index.js
@@ -4,19 +4,21 @@ const qs = Qs;
 // DOM Elements
 const $inputs = document.querySelectorAll('form input');
 const $matchBtn = document.querySelector('form button');
-const $regionSel = document.getElementById('region');
-const $teamA = document.getElementById('team-a');
-const $teamB = document.getElementById('team-b');
+const $region = document.getElementById('region');
+const $teamA = document.getElementById('team-a-players');
+const $teamB = document.getElementById('team-b-players');
+const $msg = document.getElementById('msg');
 
 // Templates
 const playerTemplate = document.getElementById('player').innerHTML;
+const errorTemplate = document.getElementById('error').innerHTML;
 
 // Populate UI from localStorage
 const populateUI = () => {
     // Region select
     const selectedRegionIndex = localStorage.getItem('selectedRegionIndex');
     if(selectedRegionIndex){
-        $regionSel.selectedIndex = selectedRegionIndex;
+        $region.selectedIndex = selectedRegionIndex;
     };
         
     // Summoner Names
@@ -29,7 +31,7 @@ const getSumNames = () => {
     const inputsArr = Array.from($inputs);
     return inputsArr.reduce( (players, input) => {
         if(!input.value) return players;
-        players.push(input.value);
+        players.push(input.value.trim());
         return players;
     },[]);
 };
@@ -62,22 +64,56 @@ const updateTeamsUI = teams =>{
 };
 
 const balance = async (region, sumNames) => {
+    if(sumNames.length < 2) return {error: 'Please enter two or more summoner names'}
     const query = qs.stringify({ region, sumNames }, { addQueryPrefix: true });
     const matchData = await axios.get( '/match' + query );
     return matchData.data;    
 };
 
+const lockUI = () => {
+    $region.setAttribute('disabled', 'disabled');
+    $matchBtn.setAttribute('disabled', 'disabled');
+    const inputsArr = Array.from($inputs);
+    inputsArr.forEach( input => input.setAttribute('disabled', 'disabled'));
+};
+
+const unLockUI = () => {
+    $region.removeAttribute('disabled');
+    $matchBtn.removeAttribute('disabled');
+    const inputsArr = Array.from($inputs);
+    inputsArr.forEach( input => input.removeAttribute('disabled'));
+};
+
+const cleanElement =  element => {
+    element.innerHTML = '';
+};
+
+const renderError = error =>{
+    const html = Mustache.render(errorTemplate, {...error});
+    cleanElement($msg);
+    $msg.insertAdjacentHTML('beforeend', html);
+};
+
 // Match
 $matchBtn.addEventListener('click', async e => {
     e.preventDefault();
+    lockUI();
     const sumNames = getSumNames();
-    const region = $regionSel.value;
+    const region = $region.value;
     const match = await balance(region, sumNames);
+    if(match.error){
+        renderError({error: match.error});
+        return unLockUI();
+    };
+    cleanElement($msg);
+    cleanElement($teamA);
+    cleanElement($teamB);
     updateTeamsUI(match);
+    unLockUI();
 });
 
 // Save select value on localStorage
-$regionSel.addEventListener('change', e => {
+$region.addEventListener('change', e => {
     const selectedRegionIndex = e.target.selectedIndex;
     localStorage.setItem('selectedRegionIndex', selectedRegionIndex);
 });

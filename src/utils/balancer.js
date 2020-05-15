1c1ca956af681d7e1b21feaf07c5e1b6619a9a12
Andres Munoz <am@andresmunozit.com>
2020-05-15 18:00:46 -0500

    Adding functions to balance a group of players, in two teams, by player mastery

--- src/utils/balancer.js
+++ src/utils/balancer.js
@@ -1,4 +1,5 @@
 const Player = require('../models/player')
+const Combinatorics = require('js-combinatorics');
 
 // sumName = summoner names
 const getPlayers = async (region, sumNames) => {
@@ -22,21 +23,56 @@ const balance = players => {
     // Todo
 };
 
-const balanceByMastery = players => {
-    const totalPlayers = players.length;
-    const match = {teamA: [], teamB: []};
+// Return the sum of mastery for an array of players (a team is an array of players)
+const sumMastery = players => {   
+    return players.reduce( (acc, player) => acc + player.mastery, 0)
+};
 
-    if(totalPlayers === 2){
-        match.teamA.push( players[0] );
-        match.teamB.push( players[1] );
-        return match;
+const balanceByMastery = players => {
+    const playersCount = players.length;
+    const match = {};
+    let possibleTeams;
+
+    if(playersCount === 2) return {teamA: [ players[0] ], teamB: [ players[1] ] };
+
+    const totalMastery = sumMastery(players);
+    const mediaMastery = totalMastery / 2;
+
+    if ( playersCount % 2 === 0 ){ // Even number of players
+        const teamSize = parseInt(playersCount / 2);
+        possibleTeams = Combinatorics.combination( players, teamSize ).toArray();
+    } else { // Odd number of players
+        const smallTeamSize = Math.floor(playersCount / 2);
+        const bigTeamSize = Math.ceil(playersCount / 2);
+
+        const possibleSmallTeams = Combinatorics.combination( players, smallTeamSize ).toArray();
+        const possibleBigTeams = Combinatorics.combination( players, bigTeamSize ).toArray();
+
+        possibleTeams = [...possibleSmallTeams, ...possibleBigTeams];
     };
 
-    const totalMastery = players.reduce( (acc, player) => acc + player.mastery, 0);
-    const media = Math.floor( totalMastery / totalPlayers );
+    // A team is an array of players
+    const distancesToMedia = possibleTeams.map( team => {
+        const teamMastery = sumMastery(team);
+        const distanceToMedia = Math.abs(mediaMastery - teamMastery);
+        return distanceToMedia;
+    });
 
+    const minDistanceToMedia = Math.min(...distancesToMedia);
+    const indexOfBestMatch = distancesToMedia.indexOf(minDistanceToMedia);
+    
+    const teamA = possibleTeams[indexOfBestMatch];
+    const teamAIDs = teamA.map( player => player.id );
+    
+    const teamB = players.filter( player => !teamAIDs.includes(player.id) );
 
-    return totalMastery;
+    match.teamA = teamA;
+    match.teamB = teamB;
+
+    return match;
 };
+
+// console.log( team, {totalMastery}, {teamMastery}, {mediaMastery}, {distanceToMedia} )
+
 
 module.exports = { getPlayers, balance, balanceByMastery };

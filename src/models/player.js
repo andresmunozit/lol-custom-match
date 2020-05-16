c09b92092b5920cdc5e1ac6c69d67c82c34e0fe8
Andres Munoz <am@andresmunozit.com>
2020-05-16 03:32:35 -0500

    Add solo queue informtion when the player synchronize

--- src/models/player.js
+++ src/models/player.js
@@ -10,12 +10,22 @@ class Player {
         this.mastery = undefined;
         this.leagues = undefined;
     };
+
+    static totalLeaguePoints(tier, rank, leaguePoints){
+        const tiers = ['IRON','BRONZE','SILVER','GOLD','PLATINUM','DIAMOND', 'MASTER', 'GRANDMASTER', 'CHALLENGER'];
+        const ranks = ['I','II','III','IV'];
+        
+        const tierPoints = tiers.indexOf(tier) * 400;
+        const rankPoints = ranks.indexOf(rank) * 100;
+
+        return tierPoints + rankPoints + leaguePoints;
+    };
 };
 
 Player.prototype.sync = async function(){
     const region = this.region;
     const name = this.name;
-    
+        
     try{
         const summonerInfo = await axios.get(api.sumByName(region, name), config);
         this.id = summonerInfo.data.id;
@@ -25,13 +35,13 @@ Player.prototype.sync = async function(){
         this.mastery = masteryData.data;
 
         const leaguesData = await axios.get(api.leaguesById(region, this.id), config);
-        this.leagues = leaguesData.data.map(league => {
-            return {
-                queueType: league.queueType ,
-                tier: league.tier ,
-                rank: league.rank ,
-                leaguePoints: league.leaguePoints ,
-            }
+        this.leagues = {};
+        leaguesData.data.forEach( league => {
+            const { tier, rank, leaguePoints, queueType } = league;
+            if(league.queueType === 'RANKED_SOLO_5x5') {
+                this.leagues.solo = { tier, rank, leaguePoints };
+                this.leagues.solo.totalLeaguePoints = Player.totalLeaguePoints( tier, rank, leaguePoints );
+            } 
         });
 
         return this;

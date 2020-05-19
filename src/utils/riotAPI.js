7f41654d2c9d7c99d407205b49a1c05e6a455b42
Andres Munoz <am@andresmunozit.com>
2020-05-19 11:06:02 -0500

    apiErrors is now a map of objects that uses the status code as index

--- src/utils/riotAPI.js
+++ src/utils/riotAPI.js
@@ -8,4 +8,27 @@ const config = {
     headers: {'X-Riot-Token':`${process.env.RIOT_API}`}
 };
 
-module.exports = {api, config};
+const regions = new Map([
+    ['br1', 'BR'],
+    ['eun1', 'EUNE'],
+    ['euw1', 'EUW'],
+    ['la1', 'LAN'],
+    ['la2', 'LAS'],
+    ['oce/oc1', 'OCE'],
+    ['tr1', 'TR'],
+    ['jp1', 'JP'],
+    ['kr', 'KR'],
+]);
+
+const apiErrors = new Map([
+    [400, {error: 'Bad request', code: 400}],
+    [401, {error: 'Unauthorized', code: 401}],
+    [403, {error: 'Forbidden', code: 403}],
+    [404, {error: 'Not found', code: 404}],
+    [415, {error: 'Unsuported Media Type', code: 415}],
+    [429, {error: 'Rate Limit Exceed', code: 429}],
+    [500, {error: 'Internal Server Error', code: 500}],
+    [503, {error: 'Service unavailable', code: 503}],
+]); 
+
+module.exports = {api, config, apiErrors, regions};
